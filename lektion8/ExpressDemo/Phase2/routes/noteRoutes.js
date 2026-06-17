000be2d1bd560194121ef6bd0732aacb542e7993
Therkl <therkelsaltoftschou@gmail.com>
2026-06-17 10:35:36 +0200

    Delete lektion8 directory

--- lektion8/ExpressDemo/Phase2/routes/noteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const noteController = require('../controllers/noteController');
-
-router.get('/', noteController.getAllNotes);
-router.post('/add', noteController.createNote);
-
-module.exports = router;
--- lektion8/solutions/Phase2 (for exercise 4)/routes/noteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const noteController = require('../controllers/noteController');
-const validateNote = require('../middlewares/validateNote'); // This part was added
-
-router.get('/', noteController.getAllNotes);
-router.post('/add', validateNote, noteController.createNote); // Note the *order* of the validation
-
-module.exports = router;
